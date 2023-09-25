6b56bbcc13aea5e87908feeb7c33b6df15add8ab
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-09-25 03:52:25 -0300

    Fix img src extension

--- src/components/Testimony.js
+++ src/components/Testimony.js
@@ -4,7 +4,7 @@ import "../stylesheets/testimony.css"
 function Testimony() {
   return (
     <div className="testimony-container">
-      <img className="testimony-img" src={require("../images/testimony-nicolas")} alt="Nicolás's portrait"/>
+      <img className="testimony-img" src={require("../images/testimony-nicolas.jpg")} alt="Nicolás's portrait"/>
       <div className="testimony-text-container">
         <p className="testimony-name">Nicolás Weibel in Argentina</p>
         <p className="testimony-position">Software Engineer at Spotify</p>
