78edb81031ab8b9e7eeaefdd1803d53af7eef9c5
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-09-26 00:16:13 -0300

    Add testimonies

--- src/App.js
+++ src/App.js
@@ -6,7 +6,27 @@ function App() {
     <div className="App">
       <div className='main-container'>
         <h1>This is what our students says about us:</h1>
-        <Testimony />
+        <Testimony 
+          fullName="Nicolás Weibel"
+          country="Argentina"
+          position="Software Engineer"
+          company="Google"
+          testimony="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo massa nibh, quis fringilla augue tincidunt sit amet. Donec sodales neque in ante tempor scelerisque. Sed vulputate pharetra lectus, at tempus massa venenatis vitae. Nunc sit amet nibh lorem. Proin."
+        />
+        <Testimony 
+          fullName="Jorge Rodriguez"
+          country="Chile"
+          position="Project Manager"
+          company="Samsung"
+          testimony="Lorem ipsum dolor sit amet, quis fringilla augue tincidunt sit amet. Sed vulputate pharetra lectus, at tempus massa venenatis vitae. Nunc sit amet nibh lorem. Proin."
+        />
+        <Testimony 
+          fullName="John Smith"
+          country="United States"
+          position="Backend Developer"
+          company="Facebook"
+          testimony="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo massa nibh, quis fringilla augue tincidunt sit amet. Donec sodales neque in ante tempor scelerisque. Sed vulputate pharetra lectus, at tempus massa venenatis vitae. Nunc sit amet nibh lorem. Proin. Donec commodo massa nibh, quis fringilla augue tincidunt sit amet"
+        />
       </div>
     </div>
   );
