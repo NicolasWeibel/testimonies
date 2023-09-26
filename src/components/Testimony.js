371552207bfab9fdc1d6f484f616880bcc796492
NicolasWeibel <123994887+NicolasWeibel@users.noreply.github.com>
2023-09-26 00:48:23 -0300

    Change Testimony component

--- src/components/Testimony.js
+++ src/components/Testimony.js
@@ -6,9 +6,9 @@ function Testimony(props) {
     <div className="testimony-container">
       <img className="testimony-img" src={require(`../images/testimony-${props.image}.jpg`)} alt={`${props.fullName}'s portrait`}/>
       <div className="testimony-text-container">
-        <p className="testimony-name">{props.fullName} in {props.country}</p>
-        <p className="testimony-position">{props.position} at {props.company}</p>
-        <p className="testimony-text">{props.testimony}</p>
+        <p className="testimony-name"><b>{props.fullName}</b> in {props.country}</p>
+        <p className="testimony-position">{props.position} at <b>{props.company}</b></p>
+        <p className="testimony-text">"{props.testimony}"</p>
       </div>
     </div>
   );
